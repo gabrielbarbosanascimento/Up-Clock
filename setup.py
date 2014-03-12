5641d9269f976388004925afac55fcc317a8516a
apandada1 <apandada1@gmail.com>
2014-03-12 21:33:11 +0530

    v4.5.1

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='up-clock',
-    version='4.5',
+    version='4.5.1',
     license='GPL-3',
     author='Archisman Panigrahi',
     author_email='apandada1@gmail.com',
