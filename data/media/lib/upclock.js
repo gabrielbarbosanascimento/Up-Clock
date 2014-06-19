be89e8b61351a86bb4f37c050418fcba7172ca12
Gabriel Nascimento <barbosanascimentogabriel@gmail.com>
2014-06-19 14:20:57 -0300

    flat slider design

--- data/media/lib/upclock.js
+++ data/media/lib/upclock.js
@@ -66,71 +66,71 @@ $(document).ready(function () {
 
 	//Color changes
 	$('.c1, .c2, .c3, .c4, .c5, .c6, .c7, .c8, .c9, .c10').click(function () {
-		if ($(this).hasClass("c1")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c1Color);
+		if ($(this).hasClass('c1')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c1Color);
 			$('.c1 img').css('display', 'block');
 			$('.c2 img, .c3 img, .c4 img, .c5 img, .c6 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c1Color);
 			}
-		} else if ($(this).hasClass("c2")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c2Color);
+		} else if ($(this).hasClass('c2')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c2Color);
 			$('.c2 img').css('display', 'block');
 			$('.c1 img, .c3 img, .c4 img, .c5 img, .c6 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c2Color);
 			}
-		} else if ($(this).hasClass("c3")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c3Color);
+		} else if ($(this).hasClass('c3')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c3Color);
 			$('.c3 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c4 img, .c5 img, .c6 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c3Color);
 			}
-		} else if ($(this).hasClass("c4")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c4Color);
+		} else if ($(this).hasClass('c4')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c4Color);
 			$('.c4 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c5 img, .c6 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c4Color);
 			}
-		} else if ($(this).hasClass("c5")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c5Color);
+		} else if ($(this).hasClass('c5')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c5Color);
 			$('.c5 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c4 img, .c6 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c5Color);
 			}
-		} else if ($(this).hasClass("c6")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c6Color);
+		} else if ($(this).hasClass('c6')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c6Color);
 			$('.c6 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c4 img, .c5 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c6Color);
 			}
-		} else if ($(this).hasClass("c7")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c7Color);
+		} else if ($(this).hasClass('c7')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c7Color);
 			$('.c7 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c4 img, .c5 img, .c6 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c7Color);
 			}
-		} else if ($(this).hasClass("c8")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c8Color);
+		} else if ($(this).hasClass('c8')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c8Color);
 			$('.c8 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c4 img, .c5 img, .c6 img, .c7 img, .c9 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c8Color);
 			}
-		} else if ($(this).hasClass("c9")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c9Color);
+		} else if ($(this).hasClass('c9')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c9Color);
 			$('.c9 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c4 img, .c5 img, .c6 img, .c7 img, .c8 img, .c10 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
 				clock1(c9Color);
 			}
-		} else if ($(this).hasClass("c10")) {
-			$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", c10Color);
+		} else if ($(this).hasClass('c10')) {
+			$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', c10Color);
 			$('.c10 img').css('display', 'block');
 			$('.c1 img, .c2 img, .c3 img, .c4 img, .c5 img, .c6 img, .c7 img, .c8 img, .c9 img').css('display', 'none');
 			if ($('.t24').hasClass('off')) {
@@ -154,48 +154,48 @@ $(document).ready(function () {
 
 	//Custom Button
 	function changeColor(choosenColor) {
-		$('.custom').css("background", "rgba(0, 0, 0, 0.3)");
+		$('.custom').css('background', 'rgba(0, 0, 0, 0.3)');
 		$('#circle').fadeIn('fast');
-		$('#i1').attr("src", "media/1.svg");
-		$('#i2').attr("src", "media/2.svg");
-		$('#i3').attr("src", "media/3.svg");
-		$('#i4').attr("src", "media/4.svg");
-		$('#i5').attr("src", "media/5.svg");
+		$('#i1').attr('src', 'media/1.svg');
+		$('#i2').attr('src', 'media/2.svg');
+		$('#i3').attr('src', 'media/3.svg');
+		$('#i4').attr('src', 'media/4.svg');
+		$('#i5').attr('src', 'media/5.svg');
 		$('.classic .switch div').animate({
 			right: 0
 		}, 300);
 		$('.classic .switch').css('background-color', 'rgba(0, 0, 0, 0.1)');
-		$('#animation, #circle, #alarm-settings, .alarm-popup').css("background", choosenColor);
+		$('#animation, #circle, #alarm-settings, .alarm-popup').css('background', choosenColor);
 		$('.c1 img, .c2 img, .c3 img, .c4 img, .c5 img, .c6 img, .c7 img, .c8 img, .c9 img, .c10 img').css('display', 'none');
 		$('.custom').css('background', 'rgba(0, 0, 0, 0.4)');
 	}
 
 	$('.custom').click(function () {
-		var color = prompt("Put the whole Hexadecimal code of your favourite color (with sharp #) ! \nHow about #bada55 ?");
+		var color = prompt('Put the whole Hexadecimal code of your favourite color (with sharp #) ! \nHow about #bada55 ?');
 		colorcode_validate(color)
 
 		function colorcode_validate(choosenColor) {
 			var regColorcode = /^(#)?([0-9a-fA-F]{3})([0-9a-fA-F]{3})?$/;
 
 			if (choosenColor === null) {
-				//If the user enters clicks "Cancel", do nothing
+				//If the user enters clicks 'Cancel', do nothing
 			} else if (choosenColor == '') {
-				//If the user enters enters nothing but clicks "Ok", do nothing
+				//If the user enters enters nothing but clicks 'Ok', do nothing
 			} else if (regColorcode.test(choosenColor) == false) {
-				alert("The code you entered is invalid. \nVisit http://html-color-codes.info/ to get correct hexadecimal code of colors.");
+				alert('The code you entered is invalid. \nVisit http://html-color-codes.info/ to get correct hexadecimal code of colors.');
 			} else {
 				changeColor(choosenColor);
-				localStorage.setItem("CHOOSEN_COLOR", choosenColor);
-				localStorage.removeItem('c1', 'true');
-				localStorage.removeItem('c2', 'true');
-				localStorage.removeItem('c3', 'true');
-				localStorage.removeItem('c4', 'true');
-				localStorage.removeItem('c5', 'true');
-				localStorage.removeItem('c6', 'true');
-				localStorage.removeItem('c7', 'true');
-				localStorage.removeItem('c8', 'true');
-				localStorage.removeItem('c9', 'true');
-				localStorage.removeItem('c10', 'true');
+				setItem('CHOOSEN_COLOR', choosenColor);
+				removeItem('c1', 'true');
+				removeItem('c2', 'true');
+				removeItem('c3', 'true');
+				removeItem('c4', 'true');
+				removeItem('c5', 'true');
+				removeItem('c6', 'true');
+				removeItem('c7', 'true');
+				removeItem('c8', 'true');
+				removeItem('c9', 'true');
+				removeItem('c10', 'true');
 				if ($('.t24').hasClass('off')) {
 					clock1(choosenColor);
 				}
@@ -203,8 +203,8 @@ $(document).ready(function () {
 		}
 	});
 
-	if (localStorage.getItem("CHOOSEN_COLOR")) {
-		var savedColor = localStorage.getItem("CHOOSEN_COLOR");
+	if (localStorage.getItem('CHOOSEN_COLOR')) {
+		var savedColor = localStorage.getItem('CHOOSEN_COLOR');
 		changeColor(savedColor);
 		if ($('.t24').hasClass('off')) {
 			clock1(savedColor);
@@ -214,30 +214,30 @@ $(document).ready(function () {
 
 	//Clock changing
 	$('.t24').click(function () {
-		var savedColor = localStorage.getItem("CHOOSEN_COLOR");
+		var savedColor = localStorage.getItem('CHOOSEN_COLOR');
 
 		if ($('.t24').hasClass('on')) {
-			if (localStorage.getItem("c1") === 'true') {
+			if (localStorage.getItem('c1') === 'true') {
 				clock1(c1Color);
-			} else if (localStorage.getItem("c2") === 'true') {
+			} else if (localStorage.getItem('c2') === 'true') {
 				clock1(c2Color);
-			} else if (localStorage.getItem("c3") === 'true') {
+			} else if (localStorage.getItem('c3') === 'true') {
 				clock1(c3Color);
-			} else if (localStorage.getItem("c4") === 'true') {
+			} else if (localStorage.getItem('c4') === 'true') {
 				clock1(c4Color);
-			} else if (localStorage.getItem("c5") === 'true') {
+			} else if (localStorage.getItem('c5') === 'true') {
 				clock1(c5Color);
-			} else if (localStorage.getItem("c6") === 'true') {
+			} else if (localStorage.getItem('c6') === 'true') {
 				clock1(c6Color);
-			} else if (localStorage.getItem("c7") === 'true') {
+			} else if (localStorage.getItem('c7') === 'true') {
 				clock1(c7Color);
-			} else if (localStorage.getItem("c8") === 'true') {
+			} else if (localStorage.getItem('c8') === 'true') {
 				clock1(c8Color);
-			} else if (localStorage.getItem("c9") === 'true') {
+			} else if (localStorage.getItem('c9') === 'true') {
 				clock1(c9Color);
-			} else if (localStorage.getItem("c10") === 'true') {
+			} else if (localStorage.getItem('c10') === 'true') {
 				clock1(c10Color);
-			} else if (localStorage.getItem("CHOOSEN_COLOR")) {
+			} else if (localStorage.getItem('CHOOSEN_COLOR')) {
 				clock1(savedColor);
 			} else if (localStorage.getItem('.c1, .c2, .c3, .c4, .c5, .c6, .c7, .c8, .c9, .c10, CHOOSEN_COLOR') === null) {
 				clock1(colorSwitch);
