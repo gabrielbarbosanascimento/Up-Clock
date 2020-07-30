859fabaf092fa281b6f7338638ca4eedae83654b
Archisman Panigrahi <apandada1@gmail.com>
2020-07-30 17:50:27 +0530

    file ---> open in setup.py - file() is not supported in Python3

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ def update_config(libdir, values = {}):
     filename = os.path.join(libdir, 'up_clock_lib/up_clockconfig.py')
     oldvalues = {}
     try:
-        fin = file(filename, 'r')
-        fout = file(filename + '.new', 'w')
+        fin = open(filename, 'r')
+        fout = open(filename + '.new', 'w')
 
         for line in fin:
             fields = line.split(' = ') # Separate variable from value
@@ -84,8 +84,8 @@ def move_desktop_file(root, target_data, prefix):
 def update_desktop_file(filename, target_pkgdata, target_scripts):
 
     try:
-        fin = file(filename, 'r')
-        fout = file(filename + '.new', 'w')
+        fin = open(filename, 'r')
+        fout = open(filename + '.new', 'w')
 
         for line in fin:
             if 'Icon=' in line:
